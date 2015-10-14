d2599a7b9c5674f2a2e760623774a647867afa4f
teckliew <teck@tekleemedia.com>
2015-10-14 15:29:05 -0700

    return promises into object array

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,27 +8,45 @@ var githubAPI = "//api.github.com/users/" + user + "/repos";
 //   }
 //   console.log(langaugeURLS);
 // })
-
+var repos = {};
 
 var getGit = {
   getURLs: function(url) {
     return $.getJSON(url).then(function(data) {
-      var languageURLs = [];
       for(var i = 0; i < data.length; i++){
-        languageURLs.push(data[i]["languages_url"]);
+        repos[data[i]["name"]] = {
+          link: data[i]["languages_url"],
+          langs: {},
+        };
       }
-      return languageURLs;
     });
   },
-  getTitles: function(url) {
+  getLangs: function(url) {
     return this.getURLs(url).then(function(urlArray) {
-      for (var i = 0; i < urlArray.length; i ++){
-        $.getJSON(urlArray[i]).then(function(data) {
-          console.log(data);
-        })
+      debugger;
+      var promises = [];
+      for (var key in repos) {
+        if (repos.hasOwnProperty(key)) {
+          promises.push($.getJSON(repos[key].link).then(function(data) {
+            repos[key].langs = data;
+          }));
+        }
       }
+
+      Q.all(promises).then(function() {
+        console.log(repos);
+
+      });
+      // for (var i = 0; i < urlArray.length; i ++){
+      //   var regExp = new RegExp();
+      //   $.getJSON(urlArray[i]).then(function(data) {
+      //     console.log(data);
+      //     //console.log(repos);
+      //   })
+      // }
     })
   }
 }
 
-getGit.getTitles(githubAPI);
+//getGit.getURLs(githubAPI);
+getGit.getLangs(githubAPI);
